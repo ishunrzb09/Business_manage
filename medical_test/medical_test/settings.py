41665ce2a19d69252d662ced42dc467d91a69e7c
ishunrzb09 <gaurav.k2143@gmail.com>
2024-01-31 17:18:57 +0530

    added jenkinsfile

--- medical_test/medical_test/settings.py
+++ medical_test/medical_test/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "medical_test.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["Templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
